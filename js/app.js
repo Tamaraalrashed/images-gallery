3389c3be5cd5f2943121cc0452ee463ae97e3ce8
Tamara <tamara_almanaseer@live.com>
2021-03-30 23:26:08 +0300

    without css

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 "use strict";
-
-$.ajax("./data/page-1.json").then((imagesData) => {
+let pageNumber=1;
+dataImage(pageNumber);
+function dataImage(no){
+$.ajax(`./data/page-${no}.json`)
+     .then((imagesData) => {
   console.log(imagesData);
   imagesData.forEach((element) => {
     let newImage = new Image(element);
     newImage.renderData();
+    
   });
   options(optionsArr);
-  $("#photo-template").remove();
 });
+}
+
 
 let arrayAll = [];
-
+console.log('ddddddddddd', arrayAll);
 let optionsArr = [];
 
 function Image(image) {
@@ -19,33 +24,123 @@ function Image(image) {
   this.image_url = image.image_url;
   this.keyword = image.keyword;
   this.horns = image.horns;
+  this.description=image.description;
   arrayAll.push(this);
   optionsArr.push(this.keyword);
 }
 
 Image.prototype.renderData = function () {
-  let photoTemplate = $("#photo-template").clone().addClass(this.keyword);
-  photoTemplate.find("h2").text(this.title);
-  photoTemplate.find("img").attr("src", this.image_url);
-  photoTemplate.find("h3").text(`Keyword: ${this.keyword}`);
-  photoTemplate.find("h4").text(`Number Of Horns: ${this.horns}`);
-  $("main").append(photoTemplate);
+
+  let imageTemplate=$('#photo-template').html();
+  let dataSet=Mustache.render(imageTemplate,this);
+  $('main').append(dataSet);
+  // console.log(dataSet);
+  return dataSet;
+
 };
+
+
 
 function options(array) {
   let preventRepeat = [...new Set(array)];
   console.log(preventRepeat);
 
   preventRepeat.forEach((element) => {
-    $("select").append(`<option value="${element}">${element}</option>`);
+    $(".filter").append(`<option value="${element}">${element}</option>`);
   });
+
 }
 
 function viewSelected() {
-  $("select").change(function () {
+  $(".filter").change(function () {
     let selectedElement = $(this).val();
+    console.log('dee', selectedElement );
     $("section").hide();
+    
     $(`.${selectedElement}`).fadeIn(100);
+
   });
 }
-viewSelected();
+ viewSelected();
+
+ $('#pageOne').click(selectedPage(1));
+ $('#pageTwo').click(selectedPage(2));
+function selectedPage(pageNum){
+return function(){
+
+  // $('#pages').change(function(){
+     arrayAll=[];
+    // console.log('wwwww', arrayAll);
+    optionsArr = [];
+    $('option').not(':eq( 0 )').remove();
+    $("section").remove();
+    // let pageNum=$(this).val();
+    // $("section").hide();
+    // pageNumber=pageNum;
+    dataImage(pageNum);
+    console.log(pageNum);
+  }
+};
+// }
+// selectedPage();
+
+$('#sortTitle').click(sortTitle);
+$('#sortNoHorns').click(sortHorns);
+
+function sortHorns(){
+  arrayAll.sort(function (a, b) {
+    return a.horns - b.horns;
+  
+    });
+    console.log(arrayAll);
+    $('section').hide();  
+//     for (let i = 0; i < arrayAll.length; i++) {
+//       const element = arrayAll[i];
+//       $('main').append(`<section id='eee'></section>`)
+// $('section').append(`<h2>${element.keyword}</h2>`);
+// // $('section').append(`<img src='${element.image_url}>`);
+//  $('section').append(`<h3>${element.title}</h3>`);
+//     }
+
+const mainPage = document.querySelector('main');
+for(let i=0; i<arrayAll.length; i++){
+  const parentElement = document.createElement('section');
+  mainPage.appendChild(parentElement);
+  const hornsElement = document.createElement('h2');
+  parentElement.appendChild(hornsElement);
+  hornsElement.textContent=`Number Of Horns: ${arrayAll[i].horns}`;
+  const imgElement = document.createElement('img');
+  parentElement.appendChild(imgElement);
+  imgElement.setAttribute( 'src', `${arrayAll[i].image_url}` );
+  const titleElement = document.createElement('h3');
+  parentElement.appendChild(titleElement);
+  titleElement.textContent=`${arrayAll[i].title}`;
+  const keywordElement = document.createElement('h4');
+  parentElement.appendChild(keywordElement);
+  keywordElement.textContent=`${arrayAll[i].keyword}`;  
+    };
+  }
+
+  function sortTitle(){
+    let x=arrayAll.sort(function (a, b) {
+      return a.title.localeCompare(b.title);
+     
+    });
+    console.log(x);
+      $('section').hide(); 
+      const mainPage = document.querySelector('main');
+for(let i=0; i<arrayAll.length; i++){
+  const parentElement = document.createElement('section');
+  mainPage.appendChild(parentElement);
+  const titleElement = document.createElement('h2');
+  parentElement.appendChild(titleElement);
+  titleElement.textContent=`${arrayAll[i].title}`;
+  const imgElement = document.createElement('img');
+  parentElement.appendChild(imgElement);
+  imgElement.setAttribute( 'src', `${arrayAll[i].image_url}` );
+  
+    };
+      
+    
+
+    } 
